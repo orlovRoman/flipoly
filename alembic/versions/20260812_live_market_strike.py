946977073ee15d00837b72cd05b30235e1e6c5f2
Roman <dr.orlov@yandex.ru>
2026-08-12 19:45:11 +0700

    fix(alembic): sequence live_market_strike after merge_heads

--- alembic/versions/20260812_live_market_strike.py
+++ alembic/versions/20260812_live_market_strike.py
@@ -1,9 +1,9 @@
-﻿"""store canonical Polymarket opening strike on live markets"""
+"""store canonical Polymarket opening strike on live markets"""
 from alembic import op
 import sqlalchemy as sa
 
 revision = "20260812_live_market_strike"
-down_revision = "20260812_lgbm_configs"
+down_revision = "20260812_merge_heads"
 branch_labels = None
 depends_on = None
 
